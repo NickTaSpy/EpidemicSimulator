1e61befab5201ebc0176229a1bf49bab62a1d76e
NickTaSpy <33528423+NickTaSpy@users.noreply.github.com>
2020-07-30 02:27:00 +0300

    ScheduleSystem now uses IJobParallelFor

--- Assets/Scripts/Human/ScheduleSystem.cs
+++ Assets/Scripts/Human/ScheduleSystem.cs
@@ -4,22 +4,38 @@ using Reese.Spawning;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
+using Unity.Jobs.LowLevel.Unsafe;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
 
 namespace Epsim.Human
 {
+    [UpdateInGroup(typeof(SimulationSystemGroup), OrderFirst = true)]
     public class ScheduleSystem : SystemBase
     {
+        private const int ArrayLength = 11;
+        private const int InnerloopBatchCount = 1;
+
+        public JobHandle ScheduleJobHandle;
+
         private DateTime DateTime;
 
-        private EntityCommandBufferSystem Barrier => World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+        private readonly EntityQueryDesc Query = new EntityQueryDesc
+        {
+            None = new ComponentType[] { typeof(NavNeedsDestination) },
+            All = new ComponentType[] { ComponentType.ReadOnly<HumanBuildingData>(), ComponentType.ReadWrite<HumanData>() }
+        };
+
+        private EntityCommandBufferSystem ECB;
 
         protected override void OnCreate()
         {
+            ECB = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
             Enabled = false;
         }
 
@@ -30,33 +46,59 @@ namespace Epsim.Human
 
         protected override void OnUpdate()
         {
+            // Time
             var deltaTime = Time.DeltaTime;
             DateTime.AddSeconds(deltaTime);
 
-            var commandBuffer = Barrier.CreateCommandBuffer().AsParallelWriter();
-
+            // ScheduleJob
             var buildingAssignmentSystem = EntityManager.World.GetOrCreateSystem<BuildingAssignmentSystem>();
-            var buildingToPosition = buildingAssignmentSystem.BuildingToPosition;
-            var buildingHeight = buildingAssignmentSystem.BuildingHeight;
+            var query = GetEntityQuery(Query);
 
-            Entities
-                .WithName("ScheduleMainJob")
-                .WithReadOnly(buildingToPosition)
-                .WithAll<HumanData>()
-                .WithNone<NavNeedsDestination>()
-                .ForEach((Entity human, int entityInQueryIndex, int nativeThreadIndex, in HumanBuildingData buildingData) =>
+            var job = new ScheduleJob()
+            {
+                BuildingToPosition = buildingAssignmentSystem.BuildingToPosition,
+                BuildingHeight = buildingAssignmentSystem.BuildingHeight,
+                CommandBuffer = ECB.CreateCommandBuffer().AsParallelWriter(),
+                Entities = query.ToEntityArray(Allocator.TempJob),
+                BuildingData = query.ToComponentDataArray<HumanBuildingData>(Allocator.TempJob)
+            };
+
+            ScheduleJobHandle = job.Schedule(ArrayLength, InnerloopBatchCount, Dependency);
+        }
+
+        private struct ScheduleJob : IJobParallelFor
+        {
+            [ReadOnly]
+            public NativeHashMap<int, float2> BuildingToPosition;
+
+            [ReadOnly]
+            public float BuildingHeight;
+
+            [ReadOnly]
+            [DeallocateOnJobCompletion]
+            public NativeArray<Entity> Entities;
+
+            [ReadOnly]
+            [DeallocateOnJobCompletion]
+            public NativeArray<HumanBuildingData> BuildingData;
+
+            [NativeDisableParallelForRestriction]
+            [WriteOnly]
+            public EntityCommandBuffer.ParallelWriter CommandBuffer;
+
+            public void Execute(int i)
+            {
+                var entity = Entities[i];
+                var buildingData = BuildingData[i];
+                
+                var workPos = BuildingToPosition[buildingData.Work];
+                CommandBuffer.AddComponent(i, entity, new NavNeedsDestination
                 {
-                    var workPos = buildingToPosition[buildingData.Work];
-                    commandBuffer.AddComponent(entityInQueryIndex, human, new NavNeedsDestination
-                    {
-                        Destination = new float3(workPos.x, buildingHeight, workPos.y)
-                    });
+                    Destination = new float3(workPos.x, BuildingHeight, workPos.y)
+                });
 
-                    commandBuffer.RemoveComponent<HumanData>(entityInQueryIndex, human); //TEMP
-                })
-                .ScheduleParallel();
-
-            Barrier.AddJobHandleForProducer(Dependency);
+                CommandBuffer.RemoveComponent<HumanData>(i, entity); //TEMP
+            }
         }
     }
 }
