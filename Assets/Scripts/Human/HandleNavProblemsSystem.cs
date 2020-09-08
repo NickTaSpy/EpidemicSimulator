9191c804f6b9ee5f6f5b92e6c73f16b9ed1bd5b7
NickTaSpy <33528423+NickTaSpy@users.noreply.github.com>
2020-09-08 20:41:38 +0300

    Update

---
+++ Assets/Scripts/Human/HandleNavProblemsSystem.cs
@@ -0,0 +1,32 @@
+﻿using Reese.Nav;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Epsim.Human
+{
+    public class HandleNavProblemsSystem : SystemBase
+    {
+        private EntityCommandBufferSystem ECB;
+
+        protected override void OnCreate()
+        {
+            ECB = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+        }
+
+        protected override void OnUpdate()
+        {
+            var commandBuffer = ECB.CreateCommandBuffer().AsParallelWriter();
+
+            Entities
+                .WithName("HandleNavProblems")
+                .WithAll<NavHasProblem>()
+                .ForEach((Entity human, int entityInQueryIndex, int nativeThreadIndex) =>
+                {
+                    commandBuffer.DestroyEntity(entityInQueryIndex, human);
+                })
+                .ScheduleParallel();
+        }
+    }
+}
